9122f6e3f0eb4e553aa4304101fd0028982ccec5
navdeep710 <navdeep710@gmail.com>
2022-04-16 09:57:27 +0530

    added new rev encoding

--- webserver/main/routes/mmi.py
+++ webserver/main/routes/mmi.py
@@ -2,6 +2,8 @@ from flask_restplus import Namespace, Resource, reqparse
 
 from main.service.mmi import fetch_tokens, get_auto_complete_by_query, get_place_info_for_eloc
 
+from webserver.main.service.mmi import get_place_info_for_latlong
+
 mmi_namespace = Namespace('mmi', description='mmi utils')
 
 @mmi_namespace.route("/fetch_tokens_for_mmi")
@@ -40,4 +42,17 @@ class GetPlacesInfoForEloc(Resource):
     def get(self):
         args = self.create_parser_with_args()
         return get_place_info_for_eloc(**args)
+
+@mmi_namespace.route("/mmi_latlong_info")
+class GetPlacesInfoForEloc(Resource):
+
+    def create_parser_with_args(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("lat", required=True)
+        parser.add_argument("long", required=True)
+        return parser.parse_args()
+
+    def get(self):
+        args = self.create_parser_with_args()
+        return get_place_info_for_latlong(**args)
 
--- webserver/main/service/mmi.py
+++ webserver/main/service/mmi.py
@@ -9,6 +9,8 @@ from main.config import get_config_by_name
 MMI_ENDPOINT = "https://outpost.mapmyindia.com/api"
 MMI_AUTO_COMPLETE_ENDPOINT = "https://atlas.mapmyindia.com/api/places/search/json"
 MMI_EXPLORE_API = "https://explore.mappls.com"
+MMI_APIS_BASE_PATH = "https://apis.mapmyindia.com/advancedmaps/v1"
+
 
 @cached(TTLCache(maxsize=1024, ttl=1800))
 def fetch_tokens():
@@ -17,24 +19,30 @@ def fetch_tokens():
         "client_id": get_config_by_name("MMI_CLIENT_ID"),
         "client_secret": get_config_by_name("MMI_CLIENT_SECRET"),
     }
-    response = requests.post(f"{MMI_ENDPOINT}/security/oauth/token",params)
+    response = requests.post(f"{MMI_ENDPOINT}/security/oauth/token", params)
     json_response = response.json()
     return json_response["access_token"]
-
 
 
 def get_auto_complete_by_query(query):
     params = {"query": query}
     headers = {"Authorization": f"Bearer {fetch_tokens()}"}
-    response = requests.get(MMI_AUTO_COMPLETE_ENDPOINT,params=params,headers=headers)
+    response = requests.get(MMI_AUTO_COMPLETE_ENDPOINT, params=params, headers=headers)
     json_response = response.json()
     return json_response["suggestedLocations"]
 
 
 def get_place_info_for_eloc(eloc):
     headers = {"Authorization": f"Bearer {fetch_tokens()}"}
-    response = requests.get(f"{MMI_EXPLORE_API}/apis/O2O/entity/{eloc}",headers=headers)
+    response = requests.get(f"{MMI_EXPLORE_API}/apis/O2O/entity/{eloc}", headers=headers)
     json_response = response.json()
     return json_response
 
+
+def get_place_info_for_latlong(lat, long):
+    params = {"lat": lat, "lng": long}
+    response = requests.get(f"{MMI_APIS_BASE_PATH}/{get_config_by_name('mmi_advance_api_api_key')}/rev_geocode",
+                            params=params)
+    response = response.json()
+    return response
 
