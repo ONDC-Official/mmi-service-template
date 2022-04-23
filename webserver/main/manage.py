5eb4f6c41a52e09fd015c1da798e0b7610a11ceb
navdeep710 <navdeep710@gmail.com>
2022-04-23 12:28:48 +0530

    added fallback on serach api

--- webserver/main/manage.py
+++ webserver/main/manage.py
@@ -4,6 +4,6 @@ from main import create_app
 
 app = create_app(os.getenv("ENV") or "dev")
 
-
+#
 # if(os.getenv("ENV") == "prod"):
 #     app.run(host="localhost", port=app.config["PORT"])
--- webserver/main/service/mmi.py
+++ webserver/main/service/mmi.py
@@ -1,8 +1,11 @@
 import json
+import logging
 import os
-
+import random
 import requests
 from cachetools import cached, LRUCache, TTLCache
+from flask_restplus import abort
+from retry import retry
 
 from main.config import get_config_by_name
 
@@ -12,24 +15,34 @@ MMI_EXPLORE_API = "https://explore.mappls.com"
 MMI_APIS_BASE_PATH = "https://apis.mapmyindia.com/advancedmaps/v1"
 
 
-@cached(TTLCache(maxsize=1024, ttl=1800))
-def fetch_tokens():
+@cached(TTLCache(maxsize=1, ttl=600))
+def fetch_tokens(random=1):
     params = {
         "grant_type": "client_credentials",
         "client_id": get_config_by_name("MMI_CLIENT_ID"),
         "client_secret": get_config_by_name("MMI_CLIENT_SECRET"),
     }
     response = requests.post(f"{MMI_ENDPOINT}/security/oauth/token", params)
-    json_response = response.json()
-    return json_response["access_token"]
+    if response.status_code == 200:
+        json_response = response.json()
+        return json_response["access_token"]
+    else:
+        print(f"status code {response.content}")
+        abort(429,"unable to fetch access tokens")
 
-
+@retry(tries=4)
 def get_auto_complete_by_query(query):
     params = {"query": query}
-    headers = {"Authorization": f"Bearer {fetch_tokens()}"}
+    headers = {"Authorization": f"Bearer {fetch_tokens(random=random.randint(1,3))}"}
     response = requests.get(MMI_AUTO_COMPLETE_ENDPOINT, params=params, headers=headers)
-    json_response = response.json()
-    return json_response.get("suggestedLocations",{})
+    if response.status_code == 200:
+        json_response = response.json()
+        return json_response.get("suggestedLocations",{})
+    else:
+        print(f"status code {response.content}")
+        print("failed fetching fresh token again")
+        raise Exception()
+
 
 
 def get_place_info_for_eloc(eloc):
